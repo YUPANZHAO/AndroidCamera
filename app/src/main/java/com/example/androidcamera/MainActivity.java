2f74cb325eea36b83735c7b2f23b52d3fda16ddd
YUPANZHAO <2332998427@qq.com>
2022-12-25 14:31:06 +0800

    权限控制

--- app/src/main/java/com/example/androidcamera/MainActivity.java
+++ app/src/main/java/com/example/androidcamera/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.androidcamera;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Permission.checkPermission(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(Permission.isPermissionGranted(this)) {
+            Log.i("PERMISSION","请求权限成功");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == Permission.REQUEST_CODE) {
+            for (int grantResult : grantResults) {
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    Log.e("Permission","授权失败！");
+                    // 授权失败，退出应用
+                    this.finish();
+                    return;
+                }
+            }
+        }
+    }
+
 }
